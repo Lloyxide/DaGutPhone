e6c47cdbba3ba37bd65d07e721829945787ca683
Thomas LAROSE <larose.thomas@(none)>
2016-11-25 16:44:37 +0100

    onche

--- THOTOHLATOMATO.java
+++ THOTOHLATOMATO.java
@@ -9,7 +9,7 @@ class Class {
 
     public static void main (String[] args){
 
-	System.out.println("Hello World");
+	System.out.println("Fayeur est un faggot de haut level, mais moins que Luke omfgomfgomfgomfgomfgomfgomfgomfg");
     }
 
 
---
+++ THOTOHLATOMATO.java~
@@ -0,0 +1,16 @@
+
+class Class {
+
+    public Class() {
+
+    }
+
+
+
+    public static void main (String[] args){
+
+	System.out.println("Hello World");
+    }
+
+
+}
